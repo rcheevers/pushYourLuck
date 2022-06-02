9c2b9fcd4e841b6c8a34a31d77886b2bb8b3ac26
Ryan Cheevers <65794698+rcheevers@users.noreply.github.com>
2022-06-01 21:42:21 -0500

    Add files via upload
    added the option to skip a round

--- pushYourLuck.py
+++ pushYourLuck.py
@@ -34,7 +34,7 @@ def retrieveResult(List):
 
 
 
-def EV(List):
+def EV(List,skip):
     EVBank = sum(List)
     if(EVBank == 91):
         return 91
@@ -43,42 +43,53 @@ def EV(List):
         return stored
     EVLower = 0
     EVHigher = 0
+    EVSkip = 0
     for x in range(1,14):
         if(x not in List):
             if(x<List[-1]):
-                EVLower += EV(List+[x])
+                EVLower += EV(List+[x],skip)
             else:
-                EVHigher += EV(List+[x])
+                EVHigher += EV(List+[x],skip)
+            if (skip):
+                EVSkip += EV(List+[x],False) 
     EVLower = EVLower/(13-len(List))
     EVHigher = EVHigher/(13-len(List))
+    EVSkip = EVSkip/(13-len(List))
     
-    return max(EVBank,EVLower,EVHigher)
+    return max(EVBank,EVLower,EVHigher,EVSkip)
 
-def action(List):
+def action(List,skip):
     EVBank = sum(List)
     if(EVBank == 91):
         return 91
     EVLower = 0
     EVHigher = 0
+    EVSkip = 0
     for x in range(1,14):
         if(x not in List):
             if(x<List[-1]):
-                EVLower += EV(List+[x])
+                EVLower += EV(List+[x],skip)
             else:
-                EVHigher += EV(List+[x])
+                EVHigher += EV(List+[x],skip)
+            if (skip):
+                EVSkip += EV(List+[x],False) 
     EVLower = EVLower/(13-len(List))
     EVHigher = EVHigher/(13-len(List))
+    EVSkip = EVSkip/(13-len(List))
     print("bank ev: ",EVBank)
     print("lower ev: ",EVLower)
     print("higher ev: ",EVHigher)
-    if(max(EVBank,EVLower,EVHigher)==EVBank):
+    print("skip ev: ",EVSkip)
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVBank):
         return "Bank"
-    if(max(EVBank,EVLower,EVHigher)==EVLower):
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVLower):
         return "Lower"
-    if(max(EVBank,EVLower,EVHigher)==EVHigher):
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVHigher):
         return "Higher"
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVSkip):
+        return "Skip"
 
-def EVApprox(List,depth):
+def EVApprox(List,depth,skip):
     EVBank = sum(List)
     if(depth == 0):
         return EVBank
@@ -89,38 +100,47 @@ def EVApprox(List,depth):
         return stored
     EVLower = 0
     EVHigher = 0
+    EVSkip = 0
     for x in range(1,14):
         if(x not in List):
             if(x<List[-1]):
-                EVLower += EVApprox(List+[x],depth-1)
+                EVLower += EVApprox(List+[x],depth-1,skip)
             else:
-                EVHigher += EVApprox(List+[x],depth-1)
+                EVHigher += EVApprox(List+[x],depth-1,skip)
+            if(skip):
+                EVSkip += EVApprox(List+[x],depth-1,False)
     EVLower = EVLower/(13-len(List))
     EVHigher = EVHigher/(13-len(List))
-    
-    return max(EVBank,EVLower,EVHigher)
+    EVSkip = EVSkip/(13-len(List))
+    return max(EVBank,EVLower,EVHigher,EVSkip)
 
-def actionApprox(List,depth):
+def actionApprox(List,depth,skip):
     EVBank = sum(List)
     if(EVBank == 91):
         return 91
     EVLower = 0
     EVHigher = 0
+    EVSkip = 0
     for x in range(1,14):
         if(x not in List):
             if(x<List[-1]):
-                EVLower += EVApprox(List+[x],depth-1)
+                EVLower += EVApprox(List+[x],depth-1,skip)
             else:
-                EVHigher += EVApprox(List+[x],depth-1)
+                EVHigher += EVApprox(List+[x],depth-1,skip)
+            if (skip):
+                EVSkip += EVApprox(List+[x],depth-1,False) 
     EVLower = EVLower/(13-len(List))
     EVHigher = EVHigher/(13-len(List))
+    EVSkip = EVSkip/(13-len(List))
     print("bank ev: ",EVBank)
     print("lower ev: ",EVLower)
     print("higher ev: ",EVHigher)
-    if(max(EVBank,EVLower,EVHigher)==EVBank):
+    print("skip ev: ",EVSkip)
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVBank):
         return "Bank"
-    if(max(EVBank,EVLower,EVHigher)==EVLower):
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVLower):
         return "Lower"
-    if(max(EVBank,EVLower,EVHigher)==EVHigher):
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVHigher):
         return "Higher"
-
+    if(max(EVBank,EVLower,EVHigher,EVSkip)==EVSkip):
+        return "Skip"
